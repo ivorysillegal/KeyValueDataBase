e1da76c9c171dd19e3a7531b593256dd4b6de8f5
HASERE <1376203676@qq.com>
2023-08-06 17:26:56 +0800

    完善了选择方法的部分 使用hashmap存储指令和对应的映射 再用反射执行方法 （对应的方法体未完善） 到目前为止很多还能优化 譬如说对于重名方法的判断

--- src.com.gduf/server/DataBaseServer.java
+++ src.com.gduf/server/DataBaseServer.java
@@ -1,16 +1,26 @@
 package server;
 
 import java.io.IOException;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.net.InetSocketAddress;
 import java.nio.ByteBuffer;
 import java.nio.channels.*;
 import java.nio.charset.Charset;
+import java.util.HashMap;
 import java.util.Iterator;
+import java.util.LinkedList;
 import java.util.Set;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 public class DataBaseServer {
+
+    private static HashMap<String, String>[] METHODS;
+//    这个HashMap存储指令和对应方法的映射 目前思路是 服务器激活时通过配置文件读取数据   TBD
+//    更新思路 创立一个hashmap的数组 数组的每一项代表一个数据类型的指令及其映射
+
     public static void main(String[] args) throws IOException {
         Selector selector = Selector.open();
         ServerSocketChannel serverSocketChannel = ServerSocketChannel.open();
@@ -50,9 +60,9 @@ public class DataBaseServer {
 
                     readOperator(selector, key, executorService);
 
-                }else if (key.isWritable()){
+                } else if (key.isWritable()) {
 
-                    writeOperator(selector,key, executorService);
+                    writeOperator(selector, key, executorService);
 
                 }
             }
@@ -84,7 +94,7 @@ public class DataBaseServer {
             public void run() {
                 // Handle read data from the client
                 try {
-                    handleReadData(clientChannel,selector);
+                    handleReadData(clientChannel, selector);
                 } catch (IOException e) {
                     System.out.println("执行读的操作的时候出错");
                     e.printStackTrace();
@@ -116,6 +126,9 @@ public class DataBaseServer {
     private static void handleReadData(SocketChannel readyChannel, Selector selector) throws IOException {
         // Handle read data from the client
         // ...
+
+//        输出的命令会在这个地方接收
+//        这里应该利用反射获取方法
         //        2 创建buffer
         ByteBuffer byteBuffer = ByteBuffer.allocate(1024);
 //        3 循环读取客户端消息
@@ -127,13 +140,56 @@ public class DataBaseServer {
             byteBuffer.flip();
 //            读取内容
             msg += Charset.forName("UTF-8").decode(byteBuffer);
-
-
         }
-//        4 将channel再次注册到选择器上 监听可读状态
+
+        String command = null;
+        String[] commands = msg.split(" ");
+        for (int i = 0, methodsLength = METHODS.length; i < methodsLength; i++) {
+//            一种数据类型的命令放在一个 String String 的hashmap中
+//            例如说 :
+//            METHOD[0] 中存放HashMap<String,String>的命令
+//            METHODS[1]中存放HashMap<String,HashMap<String,String>>的命令
+            command = METHODS[i].get(commands[0]);
+            boolean flag = false;
+            if (command != null) {
+                int length = commands.length;
+                String[] parameterValues = new String[length - 1];
+                System.arraycopy(commands, 1, parameterValues, 0, parameterValues.length);
+                try {
+                    //        遍历数组里面的指令 看一下是否有正确的指令
+                    switch (i) {
+                        case 0:
+                            ExecuteOptions(parameterValues, command, "command.StringOptions");
+                            break;
+                        case 1:
+                            ExecuteOptions(parameterValues, command, "command.LinkedListOptions");
+                            break;
+                        case 2:
+                            ExecuteOptions(parameterValues, command, "command.HashOptions");
+                            break;
+                    }
+                } catch (ClassNotFoundException | IllegalAccessException | NoSuchMethodException | InstantiationException | InvocationTargetException e) {
+                    e.printStackTrace();
+                }
+                if (!flag) {
+                    System.out.println("参数输入错误 请重新输入");
+                }
+            }
+        }
+
         readyChannel.register(selector, SelectionKey.OP_READ);
-//        5 把客户端发送信息 广播到其他客户端
-        if (msg.length() > 0)
-            System.out.println(msg);
     }
+
+
+    //    TBD
+    private static void ExecuteOptions(String[] parameterValues, String command, String className) throws ClassNotFoundException, InvocationTargetException, InstantiationException, IllegalAccessException, NoSuchMethodException {
+        Class clazz = Class.forName(className);
+        Constructor constructor = clazz.getConstructor();
+        Object o = constructor.newInstance();
+        Method method = clazz.getDeclaredMethod(command);
+        method.setAccessible(true);
+        method.invoke(o, (Object) parameterValues);
+
+    }
+
 }
